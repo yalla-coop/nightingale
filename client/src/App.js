c1feee5df3d9b5e56dbff46d94217a5ab336b6f8
asalaKM <asalamatarya@gmail.com>
2019-01-28 11:08:20 +0200

    set up react side

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
-/* eslint-disable indent */
 import React, { Component } from 'react';
 import './App.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
-    render() {
-        return (
-          // eslint-disable-next-line react/jsx-filename-extension
-          <div className="App">
-            <header className="App-header">Write here </header>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        Write here
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// eslint-disable-next-line no-undef
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    // eslint-disable-next-line react/jsx-filename-extension
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
