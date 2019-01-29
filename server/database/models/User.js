206ccfbd48543c7178d215070ce7c147281c8fac
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-29 21:15:21 +0200

    feat: create hashing password pre hook Relates #54

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema
 
@@ -23,7 +24,21 @@ const userSchema = new Schema({
     type: String,
       required: false
   }
+})
 
+// create a pre hook to hash user's password before store it in the DB
+userSchema.pre("save", async (next) => {
+  // get the plain password that user input
+  const plainPassword = this.password;
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+  bcrypt.hash(plainPassword, 8)
+    .then(hash => {
+      // store the hashed password
+      this.password = hash;
+      next()
+    })
+    .catch(next)
 })
 
 module.exports = mongoose.model("users", userSchema);
