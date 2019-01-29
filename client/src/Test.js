6e0854f19c41a155af56d29b6b3abd19a07916c7
Joe Friel <joseph.s.friel@gmail.com>
2019-01-29 12:04:32 +0000

    Test change to check codecov

---
+++ client/src/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        This is a test
+      </div>
+    );
+  }
+}
+
+export default Test;
