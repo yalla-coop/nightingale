c48280ba8aef958e9facf3fe7314415bb0b48cb1
Joe Friel <joseph.s.friel@gmail.com>
2019-01-29 15:40:44 +0000

    Move error handling below production condition

--- server/router/controllers/index.js
+++ server/router/controllers/index.js
@@ -1,3 +1,7 @@
-const controllers = require('express').Router;
+const controllers = require("express").Router();
+
+controllers.get("/test", (req, res) => {
+  res.status(200).send("Hey there");
+});
 
 module.exports = controllers;
