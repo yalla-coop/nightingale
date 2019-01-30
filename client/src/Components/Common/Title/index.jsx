dd35aa9ace8c05e95fe397b9fc7c4babe5b7dc97
asalaKM <asalamatarya@gmail.com>
2019-01-30 21:22:08 +0200

    adding title component relates #53

---
+++ client/src/Components/Common/Title/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Header, Heading } from './index.style'
+
+export default class Title extends Component {
+  render() {
+    const { value } = this.props;
+    return (
+      <Header>
+        <Heading>{value}</Heading>
+      </Header>
+    );
+  }
+}
