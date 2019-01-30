f12e71a43c360e2894b6996f0ba618c5d7a1482a
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-30 14:10:46 +0200

    feat: create dummy data Relates #54

---
+++ server/database/dummy_data/user.js
@@ -0,0 +1,15 @@
+const User = require("../models/User");
+
+const buildUser = () => {
+  const defaultUser = {
+    username: "nadia-2009",
+    name: "Nadia",
+    password: "123456",
+    school: "Primrose Hill Primary School",
+    class: "A-10",
+    birthDate: "2009-02-01"
+  }
+  return User.create(defaultUser);
+};
+
+module.exports = buildUser;
---
+++ server/database/dummy_data/weekly_event.js
@@ -0,0 +1,27 @@
+const WeeklyEvents = require("../models/WeeklyEvent");
+const Mood = require("../models/Mood")
+const User = require("../models/User")
+
+const buildWeeklyEvents = async () => {
+  // get moods 
+  const moods = await WeeklyEvents.find();
+
+  // get default user "Nadia"
+  const Nadia = await User.findOne();
+
+  const weeklyEvents = [{
+    user: Nadia,
+    days: [1, 3, 5],
+    eventEmotion: moods[0],
+    text: "Math lesson"
+  }, {
+    user: Nadia,
+    days: [2, 4],
+    eventEmotion: moods[5],
+    text: "History lesson"
+  }]
+
+  return WeeklyEvents.insertMany(weeklyEvents)
+};
+
+module.exports = buildWeeklyEvents;
