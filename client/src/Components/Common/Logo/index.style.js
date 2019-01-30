a94606998fc041455cc3b0a68b62371b7635c745
asalaKM <asalamatarya@gmail.com>
2019-01-30 11:25:51 +0200

    adding logo in the app and style it

---
+++ client/src/Components/Common/Logo/index.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const LogoDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 50%;
+    margin: 0 auto;
+`
+export const Logo = styled.img`
+    width: 10rem;
+    height: 10rem;
+`
+export const LogoTitle = styled.img`
+    width: 10rem;
+`
