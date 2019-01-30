b734ce42f8040e0537edf1dde00ecd7f0ccc227a
asalaKM <asalamatarya@gmail.com>
2019-01-30 13:17:34 +0200

    fix eslint

--- client/src/Components/Common/Button/index.jsx
+++ client/src/Components/Common/Button/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Button } from "./index.style"
+import { Button } from './index.style';
 
-export default function index(props) {
-    const { value, color, onClick } = props;
-    return (
-    <>
-      <Button type='submit' style={ { background:color } } onClick={ onClick }>
-          {value}
+export default function index(props){
+       const { value, color, onClick } = props;
+    return (<>
+      <Button type='submit' style={{ background: color }} onClick={onClick}>
+        {value}
       </Button>
     </>
-  );
+    );
+  }
 }
