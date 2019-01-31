b4fb31036ec966d1d84fe736906112e7651eaf4e
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-31 10:57:04 +0200

    feat: create passport config Relates #17

---
+++ server/passport.js
@@ -0,0 +1,31 @@
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const passport = require("passport");
+
+const User = require("./database/models/User");
+
+module.exports = () => {
+  // passport strategy options
+  const opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = process.env.SECRET;
+
+  // JWT strategy
+  const strategy = new JwtStrategy(opts, ((jwtPayload, done) => {
+    User.findById(jwtPayload)
+      .then((user) => {
+        if (user) return done(null, user);
+        return done(null, false);
+      })
+      .catch((err) => {
+        done(err, false);
+      });
+  }));
+
+  // use JWT strategy as middleware
+  passport.use(strategy);
+
+  return {
+    initialize: () => passport.initialize(),
+    authenticate: () => passport.authenticate("jwt", { session: false }),
+  };
+};
