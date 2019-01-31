3245e6964fffb8104b8cd8d6e5f0a630d9e87357
asalaKM <asalamatarya@gmail.com>
2019-01-31 11:49:59 +0200

    set up client (raect) routs

---
+++ client/src/Components/Routs/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Header from '../Common/Header';
+import Login from '../Pages/Login'  ;
+import { Container } from './index.style';
+
+export default function index() {
+    return (
+        <Header />
+      <Container>
+            <Route
+              exact
+              path="/login"
+              component={Login}
+            />
+    </Container>
+  );
+}
