1e54fb64f3f9068dea70ce0ebb58d64707e970aa
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-01-31 10:59:23 +0200

    feat: create register backend functions Relates #17

---
+++ server/router/controllers/register.js
@@ -0,0 +1,33 @@
+const createError = require("http-errors");
+
+const checkUsername = require("../../database/queries/register");
+const validation = require("./../../validation");
+const createToken = require("./create_token");
+
+module.exports = (req, res, next) => {
+  const { username, name, password } = req.body;
+
+  validation({ name, username, password }, "register")
+    .then(async () => {
+      // check the username is it unique?
+      await checkUsername({ name, username, password })
+        .then((newUser) => {
+          const data = { name, username, id: newUser.id };
+          const token = createToken(data);
+          res.cookie("token", token);
+          res.send();
+        })
+        .catch((err) => {
+          console.log(err);
+
+          if (err.message === "username already taken") {
+            // handle Bad request , duplicated username
+            return next(createError(409, "username already taken"));
+          }
+          // handle server error in DB query
+          return next(createError(500));
+        });
+    })
+    // handle Bad requests in validation
+    .catch(err => next(createError(400, err)));
+};
