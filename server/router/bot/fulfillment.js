91caf1afeb47fb96c4db088d666521737b0f5e1d
Joe Friel <joseph.s.friel@gmail.com>
2019-02-01 14:26:18 +0000

    Require the right tools from dialogflow-fulfillment

--- server/router/bot/fulfillment.js
+++ server/router/bot/fulfillment.js
@@ -1,18 +1,16 @@
-const { WebhookClient } = require("dialogflow-fulfillment");
+const { WebhookClient, Suggestion } = require("dialogflow-fulfillment");
 
 const welcomeFulfillment = (agent) => {
-  console.log("reached", agent);
   agent.add(new Suggestion("Monday"));
   agent.add(new Suggestion("Tuesday"));
   agent.add("this is another message also");
+  console.log("reached", agent);
 };
 
 module.exports = (req, res) => {
   console.log("reached");
   const agent = new WebhookClient({ request: req, response: res });
-  console.log("agent", agent);
   const intentMap = new Map();
   intentMap.set("TestFulfillment", welcomeFulfillment);
-  console.log("intent", intentMap);
   agent.handleRequest(intentMap);
 };
