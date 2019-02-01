9b33b58e98dfb4d871ad84162da2ac139fe40733
Joe Friel <joseph.s.friel@gmail.com>
2019-02-01 13:48:55 +0000

    adding console logs to debug

--- server/router/bot/fulfillment.js
+++ server/router/bot/fulfillment.js
@@ -1,6 +1,7 @@
 const { WebhookClient } = require("dialogflow-fulfillment");
 
 const welcomeFulfillment = (agent) => {
+  console.log("reached", agent);
   agent.add("this message sent by webhooks for Welcome intent");
   agent.add("this is another message also");
 };
@@ -10,7 +11,7 @@ module.exports = (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
   console.log("agent", agent);
   const intentMap = new Map();
-  intentMap.set("TestFulFillment", welcomeFulfillment);
+  intentMap.set("TestFulfillment", welcomeFulfillment);
   console.log("intent", intentMap);
   agent.handleRequest(intentMap);
 };
