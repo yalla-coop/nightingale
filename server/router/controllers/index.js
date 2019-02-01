9384ee56b2a410d865db080272227705c2b28409
Joe Friel <joseph.s.friel@gmail.com>
2019-02-01 12:04:42 +0000

    Add some route consoles to start checking heroku set up

--- server/router/controllers/index.js
+++ server/router/controllers/index.js
@@ -1,3 +1,10 @@
-const controllers = require("express").Router;
+const express = require("express");
+
+const controllers = express.Router();
+
+controllers.use("/controllerstest", (req, res) => {
+  console.log("reached");
+  res.status(200).send("control reached");
+});
 
 module.exports = controllers;
--- server/router/index.js
+++ server/router/index.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const bot = require("./bot");
 const controllers = require("./controllers");
 
-router.use("/bot", bot);
-router.use(controllers);
-
 router.use("/test", (req, res) => {
   console.log("reached");
   res.status(200).send("Hello");
 });
+
+router.use("/bot", bot);
+router.use(controllers);
 
 module.exports = router;
