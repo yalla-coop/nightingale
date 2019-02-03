5348b899ae43ae689080b27dc737cfe234b2d822
Joe Friel <joseph.s.friel@gmail.com>
2019-02-03 20:53:50 +0000

    Add different fulfillment templates

--- server/router/bot/fulfillment.js
+++ server/router/bot/fulfillment.js
@@ -1,4 +1,6 @@
-const { WebhookClient, Suggestion, Payload } = require("dialogflow-fulfillment");
+const {
+  WebhookClient, Suggestion, Payload, Card,
+} = require("dialogflow-fulfillment");
 
 const welcomeFulfillment = (agent) => {
   agent.add(new Suggestion("Monday"));
@@ -7,16 +9,62 @@ const welcomeFulfillment = (agent) => {
   console.log("welcome reached");
 };
 
+const quickReply = (agent) => {
+  console.log("quick reached");
+  agent.add(new Suggestion("Suggestion to go here"));
+
+  agent.add(new Suggestion("Suggestion 2 to go here"));
+
+  agent.add(new Suggestion("Suggestion 3 to go here"));
+};
+
+const cardReply = (agent) => {
+  console.log("card reached");
+  const card = new Card({
+    title: "card title",
+    text: "card text",
+    imageUrl: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+    buttons: [
+      {
+        postback: "hello",
+        text: "one",
+      },
+      {
+        postback: "hello",
+        text: "two",
+      },
+      {
+        postback: "hello",
+        text: "three",
+      },
+      {
+        postback: "hello",
+        text: "four",
+      },
+      {
+        postback: "hello",
+        text: "five",
+      },
+      {
+        postback: "hello",
+        text: "six",
+      },
+    ],
+  });
+  agent.add(card);
+};
+
 const multiChoice = (agent) => {
   console.log("multi reached");
   const options = ["Monday", "Tuesday"];
-  options.map(option => agent.add(new Suggestion(option)));
+
   const payload = new Payload("multi", {
     text: "anything",
     quick_replies: options.map(option => ({
       content_type: "text",
       title: option,
       payload: option,
+      sendAsMessage: true,
     })),
   });
 
@@ -36,5 +84,7 @@ module.exports = (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
   const intentMap = new Map();
   intentMap.set("TestFulfillment", multiChoice);
+  intentMap.set("CardTemplate", cardReply);
+  intentMap.set("QuickTemplate", quickReply);
   agent.handleRequest(intentMap);
 };
