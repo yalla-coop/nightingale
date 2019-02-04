92d9a1deab8c641f4cd914436c374995980b2dd5
asalaKM <asalamatarya@gmail.com>
2019-02-04 13:45:27 +0200

    edit general layout style

--- client/src/Components/Routs/index.js
+++ client/src/Components/Routs/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../Common/Header';
 import Login from '../Pages/Login'  ;
+import Conversations from '../Pages/Conversations'  ;
 import { Container } from './index.style';
 
 export default function index() {
@@ -13,6 +14,11 @@ export default function index() {
               exact
               path="/login"
               component={Login}
+            />
+            <Route
+              exact
+              path="/:id/conversations"
+              component={Conversations}
             />
     </Container>
       </>
--- client/src/Components/Routs/index.style.js
+++ client/src/Components/Routs/index.style.js
@@ -2,4 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     margin: 3rem;
+    @media (max-width: 768px) {
+        margin: 1rem;
+    }
 `
