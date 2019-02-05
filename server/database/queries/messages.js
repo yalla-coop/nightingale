d1f7ad2e8c73b3dd6e3ab9c5c4ce9ed4f34a1d41
asalaKM <asalamatarya@gmail.com>
2019-02-05 14:18:18 +0200

    add query to get the conversation masseges

--- server/database/queries/messages.js
+++ server/database/queries/messages.js
@@ -1,11 +1,11 @@
 const mongoos = require("mongoose");
 const Messages = require("../models/Message");
 
-module.exports = id => new Promise((resolve, reject) => {
+module.exports = conversationId => new Promise((resolve, reject) => {
   Messages.aggregate([
     {
       $match: {
-        conversation: mongoos.Types.ObjectId(id),
+        conversation: mongoos.Types.ObjectId(conversationId),
       },
     },
   ])
