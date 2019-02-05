77c0665b698d4a55c4a8b4634b127d882ecd7507
asalaKM <asalamatarya@gmail.com>
2019-02-05 14:17:03 +0200

    add new rout for conversation massages

--- server/router/controllers/index.js
+++ server/router/controllers/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const login = require("./login");
 const register = require("./register");
 const conversations = require("./conversations");
+const messages = require("./messages");
 
 
 router.post("/user/register", register);
-router.get("/user/:id/conversations", conversations.getConversations);
+router.get("/user/:id/conversations", conversations);
 router.post("/user/login", login);
+router.get("/user/:id/conversations/:conversationId", messages);
 
 module.exports = router;
