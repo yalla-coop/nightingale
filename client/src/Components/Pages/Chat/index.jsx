65fcfb3dcc3d420283a43c09cdf714cffd355404
Simon Dupree <dupreenator@gmail.com>
2019-02-05 18:03:23 +0100

    react chat component botMsg added

--- client/src/Components/Pages/Chat/index.jsx
+++ client/src/Components/Pages/Chat/index.jsx
@@ -8,6 +8,7 @@ class Chat extends Component {
   // userMessage contains user input
   // conversation holds each message in conversation
   state = {
+    botMessage: "",
     userMessage: "",
     conversation: []
   };
@@ -17,15 +18,15 @@ class Chat extends Component {
       encrypted: true
     });
     // listening for the bot-response event on the bot channel, event gets triggered on the server and pass the response of the bot through the event payload.
-
     const channel = pusher.subscribe("bot");
     channel.bind("bot-response", data => {
-      const msg = {
+      const botMsg = {
         text: data.message,
         user: "ai"
       };
       this.setState({
-        conversation: [...this.state.conversation, msg]
+        botMessage: botMsg.text,
+        conversation: [...this.state.conversation, botMsg]
       });
     });
   }
@@ -39,13 +40,13 @@ class Chat extends Component {
     // after POST request, clearing the input field by setting the value of userMessage to an empty string.
     if (!this.state.userMessage.trim()) return;
 
-    const msg = {
+    const msgHuman = {
       text: this.state.userMessage,
       user: "human"
     };
 
     this.setState({
-      conversation: [...this.state.conversation, msg]
+      conversation: [...this.state.conversation, msgHuman]
     });
 
     axios.post("http://localhost:8080/api/bot/chat", {
@@ -56,6 +57,7 @@ class Chat extends Component {
   };
 
   render() {
+    console.log(this.state);
     const ChatBubble = (text, i, className) => {
       return (
         <div key={`${className}-${i}`} className={`${className} chat-bubble`}>
