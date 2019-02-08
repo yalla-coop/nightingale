2b41602747d7687a2ce2487782668123c4ea394f
Simon Dupree <dupreenator@gmail.com>
2019-02-08 13:49:17 +0100

    pusher statement updated for travis

--- server/router/bot/messages.js
+++ server/router/bot/messages.js
@@ -18,28 +18,24 @@ module.exports = async (req, res) => {
       // STORAGE ------------------------------------
       storeMessages(result.queryText, messageArr).catch(err => console.log(err));
 
-      // don't run pusher in test currently as it somehow crashes travis
-      if (!process.env.NODE_ENV === "test") {
-        // RENDER---------------------------------------
-        // check if result comes back defined and includes intent
-        if (result && result.intent) {
-          // send over array of fullfilment messages via pusher
-          await pusher("bot", "bot-response", {
-            message: messageArr,
-          });
-        } else {
-          console.log("  No intent matched.");
-        }
-        if (result.sentimentAnalysisResult) {
-          console.log("Detected sentiment");
-          console.log(`  Score: ${result.sentimentAnalysisResult.queryTextSentiment.score}`);
-          console.log(
-            `  Magnitude: ${result.sentimentAnalysisResult.queryTextSentiment.magnitude}`,
-          );
-        } else {
-          console.log("No sentiment Analysis Found");
-        }
+      // RENDER---------------------------------------
+      // check if result comes back defined and includes intent
+      if (result && result.intent) {
+        // send over array of fullfilment messages via pusher
+        await pusher("bot", "bot-response", {
+          message: messageArr,
+        });
+        console.log("pusher hiiiii");
+      } else {
+        console.log("  No intent matched.");
       }
+      // if (result.sentimentAnalysisResult) {
+      //   console.log("Detected sentiment");
+      //   console.log(`  Score: ${result.sentimentAnalysisResult.queryTextSentiment.score}`);
+      //   console.log(`  Magnitude: ${result.sentimentAnalysisResult.queryTextSentiment.magnitude}`);
+      // } else {
+      //   console.log("No sentiment Analysis Found");
+      // }
       return res.sendStatus(200);
     })
     .catch(err => res.sendStatus(500));
