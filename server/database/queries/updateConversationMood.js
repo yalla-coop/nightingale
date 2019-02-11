1229735a608802ebfc2146e7226135393549a10d
asalaKM <asalamatarya@gmail.com>
2019-02-12 00:34:55 +0200

    add

--- server/database/queries/updateConversationMood.js
+++ server/database/queries/updateConversationMood.js
@@ -3,6 +3,7 @@ const conversation = require("../models/Conversation");
 
 module.exports = (id, mood) => new Promise((resolve, reject) => {
   console.log("ID Q", id, "MOOD Q", mood);
+  console.log(typeof mongoos.Types.ObjectId(id));
   conversation.updateOne(
     { user: mongoos.Types.ObjectId(id) },
     { $set: { mood } },
