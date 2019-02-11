c5a8ac2a2bade21b68068b43d35390842e99e716
asalaKM <asalamatarya@gmail.com>
2019-02-12 00:16:57 +0200

    add

--- server/database/queries/updateConversationMood.js
+++ server/database/queries/updateConversationMood.js
@@ -1,15 +1,15 @@
-const conversation = require("../models/Conversation");
 const mongoos = require("mongoose");
+const conversation = require("../models/Conversation");
 
 module.exports = (id, mood) => new Promise((resolve, reject) => {
-  console.log("ID Q", id, "MOOD Q", mood )
+  console.log("ID Q", id, "MOOD Q", mood);
   conversation.updateOne(
-    { user: mongoos.Types.ObjectId(id)},
+    { user: mongoos.Types.ObjectId(id) },
     { $set: { mood } },
   )
     .then((result) => {
       resolve(result);
+      console.log("UPDATE RESULT", result);
     })
-    .catch(error => reject("ERRROR",error));
+    .catch(error => reject("ERRROR", error));
 });
-
