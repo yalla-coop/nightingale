18cbeea73d14d3d0bada61abac7565c385e2d6ba
asalaKM <asalamatarya@gmail.com>
2019-02-11 16:21:20 +0200

    add console .log to test function

--- server/database/queries/updateConversationMood.js
+++ server/database/queries/updateConversationMood.js
@@ -2,8 +2,9 @@ const conversation = require("../models/Conversation");
 const mongoos = require("mongoose");
 
 module.exports = (id, mood) => new Promise((resolve, reject) => {
+  console.log("ID Q" ,id, "MOOD Q", mood )
   conversation.updateOne(
-    { user: mongoos.Types.ObjectId(id), completed: false },
+    { user: mongoos.Types.ObjectId(id)},
     { $set: { mood } },
   )
     .then((result) => {
