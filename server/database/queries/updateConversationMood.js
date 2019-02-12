824850d000fa4a48872c7f6720a6caa935d64f9e
asalaKM <asalamatarya@gmail.com>
2019-02-12 11:02:03 +0200

    add

--- server/database/queries/updateConversationMood.js
+++ server/database/queries/updateConversationMood.js
@@ -2,15 +2,14 @@ const mongoos = require("mongoose");
 const conversation = require("../models/Conversation");
 const Mood = require("../models/Mood");
 
-module.exports = moodIndex => new Promise(async (resolve, reject) => {
+module.exports = (id, moodIndex) => new Promise(async (resolve, reject) => {
   const moods = await Mood.find();
 
   // console.log("moodIndex Q", moodIndex);
   // conversation.findOne()
   //   .then(res => console.log("resss", res));
-
   conversation.updateOne(
-    { completed: false },
+    { user: id, completed: false },
     { $set: { mood: moods[moodIndex].id } },
   )
     .then((result) => {
