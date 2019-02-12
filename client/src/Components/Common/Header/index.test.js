ad9cbf3a8a4b459c8aa26459492be9e2db98e887
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-02-12 15:20:40 +0200

    test: update the front-end tests

--- client/src/Components/Common/Header/index.test.js
+++ client/src/Components/Common/Header/index.test.js
@@ -1,10 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Header from "./index.jsx";
+import { StaticRouter } from "react-router-dom";
 
 describe("Header", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<Header />).toJSON();
+    const context = {};
+    const tree = renderer
+      .create(
+        <StaticRouter context={context}>
+          <Header />
+        </StaticRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
