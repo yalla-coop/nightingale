03e89b6825ddad57d82008e500a9c47637bb3c0e
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-02-12 15:18:12 +0200

    feat: create logout function Relates #36

--- server/router/controllers/index.js
+++ server/router/controllers/index.js
@@ -8,9 +8,11 @@ const conversations = require("./conversations");
 const messages = require("./messages");
 const dashboard = require("./dashboard");
 const auth = require("./../../passport")();
+const logout = require("./logout");
 
 router.post("/user/register", register);
 router.post("/user/login", login);
+router.use("/user/logout", logout);
 router.get("/user/:id/conversations/:conversationId", messages);
 
 // authenticated routes
