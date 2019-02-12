141a67f197fff98d4839bb8790c13fc57b9ac9ec
asalaKM <asalamatarya@gmail.com>
2019-02-12 15:35:41 +0200

    pull master

--- server/database/queries/updateConversationMood.js
+++ server/database/queries/updateConversationMood.js
@@ -7,13 +7,12 @@ module.exports = (id, moodIndex) => new Promise(async (resolve, reject) => {
   // console.log("MOODS", moods);
   console.log("MOOD index", moods[moodIndex]._id);
 
-  // Mood.find().then((res) => {
-  //   console.log(res, "moods 22222222222");
-  // });
-  // const id = moods[moodIndex]._id;
-  // console.log("moodIndex Q", moodIndex);
-  // conversation.findOne()
-  //   .then(res => console.log("resss", res));
+  Mood.find().then((res) => {
+    console.log(res, "moods 22222222222");
+  });
+  console.log("moodIndex Q", moodIndex);
+  conversation.findOne()
+    .then(res => console.log("resss", res));
   conversation.updateOne(
     { user: mongoos.Types.ObjectId(id), completed: false },
     { $set: { mood: mongoos.Types.ObjectId(moods[moodIndex]._id) } },
