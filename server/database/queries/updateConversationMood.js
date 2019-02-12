dbdd1a9a95a7612fb486a28b32cf495c6c2d859b
asalaKM <asalamatarya@gmail.com>
2019-02-12 16:34:05 +0200

    pull master

--- server/database/queries/updateConversationMood.js
+++ server/database/queries/updateConversationMood.js
@@ -4,29 +4,14 @@ const Mood = require("../models/Mood");
 
 module.exports = (id, moodIndex) => new Promise(async (resolve, reject) => {
   const moods = await Mood.find();
-  // console.log("MOODS", moods);
-  console.log("MOOD index", moods[moodIndex]._id);
-
-  Mood.find().then((res) => {
-    console.log(res, "moods 22222222222");
-  });
-  console.log("moodIndex Q", moodIndex);
-  conversation.findOne()
-    .then(res => console.log("resss", res));
   conversation.updateOne(
     { user: mongoos.Types.ObjectId(id), completed: false },
     { $set: { mood: mongoos.Types.ObjectId(moods[moodIndex]._id) } },
   )
     .then((result) => {
-      conversation.findOne({ completed: false }).then((results) => {
-        console.log("ramy", results);
-      });
       resolve(result);
-      console.log("UPDATE RESULT", result);
     })
     .catch((error) => {
-      console.log(error, "FIRST ERROR");
-
       reject(error);
     });
 });
