e88a9fc4b158faf2bf1c19571a8e2301b359216e
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-02-13 12:55:26 +0200

    fix: add expire date to the token Relates #113

--- server/router/controllers/create_token.js
+++ server/router/controllers/create_token.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (data) => {
-  const token = jwt.sign(JSON.stringify(data), process.env.SECRET);
+  const token = jwt.sign(data, process.env.SECRET, { expiresIn: "1d" });
   return token;
 };
