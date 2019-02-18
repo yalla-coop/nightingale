da911ecb2f2e7d10f39ae08dfdddfddfdf04c2fe
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-02-18 16:32:14 +0200

    log the process env

--- server/router/bot/pusherSessionClient.js
+++ server/router/bot/pusherSessionClient.js
@@ -2,6 +2,11 @@ const Pusher = require("pusher");
 
 module.exports = (channel, action, message) => new Promise((resolve, reject) => {
   // setup pusher config
+  console.log(process.env.PUSHER_APP_ID, "process.env.PUSHER_APP_ID");
+  console.log(process.env.PUSHER_APP_KEY, "process.env.PUSHER_APP_KEY");
+  console.log(process.env.PUSHER_APP_SECRET, "process.env.PUSHER_APP_SECRET");
+  console.log(process.env.PUSHER_APP_CLUSTER, "process.env.PUSHER_APP_CLUSTER");
+
   const pusher = new Pusher({
     appId: process.env.PUSHER_APP_ID,
     key: process.env.PUSHER_APP_KEY,
